22d573fad75428d688e58cee84d38be25f475787
Kenndy <kennerdeigh@me.com>
2013-12-16 15:52:11 +0000

    Added Database cleaner gem, and configured RSpec to use it

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ gem 'sinatra'
 gem 'shotgun'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
 
 group :devlopment, :test do 
   gem "rspec"
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,16 @@ ENV["RACK_ENV"] = 'test' #because we need to know what database to work with
 #needs to know what environment it's running: test or devleopment.
 #The environment determines what database to use.
 require './server'
+
+config.before(:suite) do
+  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.clean_with(:truncation)
+end
+
+config.before(:each) do
+  DatabaseCleaner.start
+end
+
+confir.after(:each) do
+  DatabaseCleaner.clean
+end
