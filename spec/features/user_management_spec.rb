1d99586a70ff0779b3754a229ec35f4b33120b97
Kenndy <kennerdeigh@me.com>
2013-12-17 17:01:00 +0000

    Added Password Confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -19,11 +19,18 @@ feature "User signs up" do
   end
 
   def sign_up(email = "ken@example.com",
-              password = "oranges!")
+              password = "oranges!",
+              password_confirmation = "oranges!")
   visit 'users/new'
   fill_in :email, :with => email
   fill_in :password, :with => password
+  fill_in :password_confirmation, :with => password_confirmation
   click_button "Sign up"
   end
+
+  scenario "with a password that doesn't match" do
+    lambda { sign_up('a@a.com', 'pass', 'wrong')}.should change(User, :count).by(0)
+  end
+
 
 end
